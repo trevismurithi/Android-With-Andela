520f8d0b8d077179be82baafb97dcececc6a3aee
trevismurithi <40691110+trevismurithi@users.noreply.github.com>
2019-07-15 22:32:20 +0300

    Update WebActivity.java

--- app/src/main/java/com/trevis/andelaproject/WebActivity.java
+++ app/src/main/java/com/trevis/andelaproject/WebActivity.java
@@ -20,11 +20,51 @@ public class WebActivity extends AppCompatActivity {
         setContentView(R.layout.activity_web);
 
         webView = (WebView) findViewById(R.id.web_page_andela);
-        webView.setWebViewClient(new WebViewClient());
-            webView.loadUrl("https://learning-digest.andela.com/");
 
-            WebSettings webSettings = webView.getSettings();
-            webSettings.setJavaScriptEnabled(true);
+
+            webView.getSettings().setJavaScriptEnabled(true);
+            webView.getSettings().setDomStorageEnabled(true);
+            webView.getSettings().setLoadsImagesAutomatically(true);
+            webView.getSettings().setAllowContentAccess(true);
+            webView.setVerticalScrollBarEnabled(true);
+            webView.setHorizontalScrollBarEnabled(false);
+            webView.getSettings().setAppCacheEnabled(true);
+            webView.getSettings().setDisplayZoomControls(false);
+            webView.getSettings().setDatabaseEnabled(true);
+            webView.getSettings().setLoadWithOverviewMode(true);
+            webView.getSettings().setUseWideViewPort(true);
+            webView.getSettings().setBuiltInZoomControls(true);
+            webView.setScrollBarStyle(View.SCROLLBARS_INSIDE_OVERLAY);
+
+            webView.setWebViewClient(new WebViewClient(){
+
+                @Override
+                public void onReceivedSslError(WebView view, SslErrorHandler handler, SslError error) {
+                    handler.proceed();
+                }
+
+                @Override
+                public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
+                    return false;
+                }
+            });
+
+            webView.loadUrl("https://andela.com/alc");
+        }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        onBackPressed();
+        return true;
+    }
+
+    @Override
+    public void onBackPressed() {
+        if(webView.canGoBack()){
+            webView.goBack();
+        }else {
+            super.onBackPressed();
+        }
 
     }
 
